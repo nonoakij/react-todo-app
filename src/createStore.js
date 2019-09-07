40518e09b970e8f1e5ee42df667a78653ffdc5bc
nonoakij <matsumoto_aki@camobile.com>
2019-09-07 19:14:55 +0900

    read me 書き足した

--- src/createStore.js
+++ src/createStore.js
@@ -2,6 +2,14 @@ import { createStore as reduxCreateStore, applyMiddleware, combineReducers } fro
 import logger from "redux-logger";
 import { todoReducer } from "./reducers/Todo";
 
+/**
+ * 
+ * Redux内にある同名の createStore という関数を内部で使用するので、as を使って “reduxCreateStore” という名前で import しています。
+ * 基本的には複数のReducerを利用することになるので、あらかじめ combineReducers を使って複数のReducerを使えるようにしています。
+ * また、開発中は便利なので applyMiddleware で logger を適応しておくようにします。
+ * 
+ */
+
 export default function createStore() {
   const store = reduxCreateStore(
     combineReducers({
